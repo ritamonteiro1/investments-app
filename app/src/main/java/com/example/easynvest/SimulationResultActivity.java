e1cb4b55b07911ca3d5177d99366f6206a16008b
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-05-20 15:52:12 -0300

    easynvest initial project

---
+++ app/src/main/java/com/example/easynvest/SimulationResultActivity.java
@@ -0,0 +1,14 @@
+package com.example.easynvest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimulationResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simulation_result);
+    }
+}
